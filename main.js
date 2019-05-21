2ad00074d966d3d4fe66d836984f78c9161708dc
Leonardino80 <gilgamesh2005@gmail.com>
2019-05-21 23:27:32 +0200

    fatto esercizio con funzione interna. da provare ancora con ajax

--- main.js
+++ main.js
@@ -1,31 +1,59 @@
 $(document).ready(function(){
 
+// funzione per ottenere numeri casuali
+function getRandomNumber(low, high) {
+  var r = Math.floor(Math.random() * (high - low + 1)) + low;
+  return r;
+}
+
+// CREO IL TEMPLATE CON Handlebars
 var source = $('#quadratino-Handlebars').html();
-// console.log(source);
 var template = Handlebars.compile(source);
-// console.log(template);
 
+// FISSO I CAMPI COMPILABILI DI HANDLEBARS
 var context = {
-  'title' : "numerino"
+  'number' : getRandomNumber(0 , 9)
 };
-
 var html = template(context);
 
+// appendo i quadratini nel container
 for (var i = 0; i < 36; i++) {
-$('.container').append(html);
+  $('.container').append(html);
 }
-// console.log(html);
+// ciclo per aggiungere i numeri random(var numerorandom dentro il ciclo!)
+$('.quadrato').each( function (){
+  // invoco la funzione dentro il ciclo così ottengo numeri diversi
+  var numero_random = getRandomNumber(0, 9)
+  $(this).data('attribute', numero_random);
+});
+
+
+// clicco ogni quadrato, se il numero è <=5 diventa giallo
+// altrimenti diventa verde
+$(document).on('click', '.quadrato', function(){
+  var numero = $(this).data('attribute');
+  if ( numero <= 5) {
+    $(this).addClass('yellow');
+  } else{
+    $(this).addClass('green');
+  }
+  // mostro il numero trigger, sia esso giallo o verde.
+  $(this).text(numero);
 })
 
+$.ajax({
+url :
+"https://www.boolean.careers/api/random/int",
+method : “GET”,
+success : function (data,stato) {
+$("#risultati").html(data);
+},
+error : function (richiesta,stato,errori) {
+alert("E' avvenuto un errore. "+errore);
+}
+});
 
-// $.ajax({
-// url :
-// "https://www.boolean.careers/api/random/int",
-// method : “GET”,
-// success : function (data,stato) {
-// $("#risultati").html(data);
-// },
-// error : function (richiesta,stato,errori) {
-// alert("E' avvenuto un errore. "+errore);
-// }
-// });
+
+
+
+})
